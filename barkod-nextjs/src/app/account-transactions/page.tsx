bcd35c47044748fea16c856b295444def093febb
Aliburus <aliburus1905@gmail.com>
2025-07-24 17:30:57 +0300

    .

---
+++ barkod-nextjs/src/app/account-transactions/page.tsx
@@ -0,0 +1,4 @@
+import AccountTransactionsPage from "../../components/Pages/AccountTransactionsPage";
+export default function Page() {
+  return <AccountTransactionsPage />;
+}
---
+++ barkod-nextjs/src/app/customers/page.tsx
@@ -0,0 +1,4 @@
+import CustomersPage from "../../components/Pages/CustomersPage";
+export default function Page() {
+  return <CustomersPage />;
+}
