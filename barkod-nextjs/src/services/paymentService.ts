9851ba2d6fdd3c93c3a3bef5ad6e11190eba6921
Aliburus <aliburus1905@gmail.com>
2025-07-24 19:34:52 +0300

    .

--- barkod-nextjs/src/services/paymentService.ts
+++ barkod-nextjs/src/services/paymentService.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import type { Payment } from "../types";
 
 export const paymentService = {
-  getAll: async () => {
+  getAll: async (): Promise<Payment[]> => {
     const res = await axios.get("/api/payments");
     return res.data;
   },
-  create: async (payment: any) => {
+  create: async (payment: Payment): Promise<Payment> => {
     const res = await axios.post("/api/payments", payment);
     return res.data;
   },
